d14e0fd80a7ad15009eff78c5a6cacc9a1116e31
leonardo <modlfo@gmail.com>
2015-01-14 22:23:00 +0100

    Implements the first version of the stack-based debuger

--- debugger.ml
+++ debugger.ml
@@ -24,6 +24,8 @@ THE SOFTWARE.
 
 open Types
 open Lexing
+open TypesUtil
+open PrintTypes
 
 (** Return values of the interpreter *)
 type value =
@@ -32,6 +34,20 @@ type value =
    | VNum     of float
    | VBool    of bool
    | VTuple   of value list
+
+type instruction =
+   | Value  of value
+   | Reg    of named_id
+   | Lazy   of instruction list
+   | Call   of named_id
+   | Obj    of int
+   | Lambda of named_id list
+   | Store
+   | Read
+   | If
+   | Ret
+   | Drop
+   | Loop
 
 module StringMap = Map.Make(String)
 
@@ -63,11 +79,6 @@ type breakpoints =
       lines : int list;
    }
 
-let apply_default (f:'a -> 'b) (v:'a option) (def:'b) =
-   match v with
-   | Some(x) -> f x
-   | _ -> def
-
 (** Joins a list of strings *)
 let rec joinStrings sep elems =
    match elems with
@@ -75,13 +86,134 @@ let rec joinStrings sep elems =
    | [h] -> h
    | h::t -> h^sep^(joinStrings sep t)
 
-(* Step, Step-in, Step-out *)
+(** Converts a value to string *)
+let rec valueStr (value:value) : string =
+   match value with
+   | VUnit         -> "()"
+   | VNum(v)       -> string_of_float v
+   | VString(s)    -> "\""^s^"\""
+   | VBool(true)   -> "true"
+   | VBool(false)  -> "false"
+   | VTuple(elems) ->
+      let elems_s = elems
+                    |> List.map valueStr
+                    |> joinStrings ","
+      in "("^elems_s^")"
 
-let isNextLine state stmt =
-   let location = TypesUtil.getExpLocation stmt in
-   location.start_pos.pos_lnum > state.line
+let append_nl buff s =
+   let _ = append buff ": " in
+   let _ = append buff s in
+   newline buff
 
-let isPrevLine state stmt =
-   let location = TypesUtil.getExpLocation stmt in
-   location.start_pos.pos_lnum < state.line
+let rec printIBuff buff i =
+   match i with
+   | Value(v)  -> append_nl buff (valueStr v)
+   | Reg(name) -> append_nl buff ("$"^namedIdStr name)
+   | Store     -> append_nl buff "Store"
+   | Read      -> append_nl buff "Read"
+   | Call(name)-> append_nl buff ("Call("^(namedIdStr name)^")")
+   | Obj(n)    -> append_nl buff ("Obj("^(string_of_int n)^")")
+   | Ret       -> append_nl buff "Ret"
+   | If        -> append_nl buff "If"
+   | Drop      -> append_nl buff "Drop"
+   | Lambda(vars) -> append_nl buff ("Lambda("^(joinStrings "," (List.map namedIdStr vars))^")")
+   | Loop      -> append_nl buff "Loop"
+   | Lazy(il)  ->
+      append buff ": [";
+      indent buff;
+      printIBuffList buff il;
+      outdent buff;
+      append buff "]";
+      newline buff
+and printIBuffList buff il =
+   printList buff printIBuff "" il
+
+let printInstructions il =
+   let buffer = makePrintBuffer () in
+   let _ = printIBuffList buffer il in
+   print_string (contents buffer)
+
+let rec assemble (i0:instruction list) (exp:parse_exp) =
+   match exp with
+   | PUnit(_)     -> Value(VUnit)::i0
+   | PBool(v,_)   -> Value(VBool(v))::i0
+   | PInt(v,_)    -> Value(VNum(float_of_string v))::i0
+   | PReal(v,_)   -> Value(VNum(float_of_string v))::i0
+   | PId(name)    -> Read::Reg(name)::i0
+   | PTuple(el,_) ->
+      let n = List.length el in
+      let i1 = assembleListExp i0 el in
+      Obj(n)::i1
+   | PCall(fname,args,_,_) ->
+      let i1 = assembleListExp i0 args in
+      Call(fname)::i1
+   | PSeq(el,_) -> assembleListStmt i0 el
+   | PIf(cond,then_,else_,_) ->
+      let i1 = assemble i0 cond in
+      let then_i = Lazy(assemble [] then_) in
+      let else_i = Lazy(assemble [] else_) in
+      If::else_i::then_i::i1
+   | StmtVal(PId(name),None,_) ->
+      Store::Reg(name)::Value(VNum(0.0))::i0
+   | StmtVal(PId(name),Some(init),_) ->
+      let i1 = assemble i0 init in
+      Store::Reg(name)::i1
+   | StmtMem(PId(name),None,None,_) ->
+      Store::Reg(name)::Value(VNum(0.0))::i0
+   | StmtMem(PId(name),Some(init),None,_) ->
+      let i1 = assemble i0 init in
+      Store::Reg(name)::i1
+   | StmtMem(PId(name),_,Some(init),_) ->
+      let i1 = assemble i0 init in
+      Store::Reg(name)::i1
+   | StmtVal(_,_,_) -> failwith "Complex bindings should be simplified"
+   | StmtMem(_,_,_,_) -> failwith "Complex bindings should be simplified"
+   | StmtBind(PId(name),e2,loc) ->
+      let i1 = assemble i0 e2 in
+      Store::Reg(name)::i1
+   | StmtBind(PUnit(_),e2,loc) ->
+      assemble i0 e2
+   | StmtBind(_,_,_) -> failwith "Complex bindings should be simplified"
+   | StmtReturn(e,_) ->
+      let i1 = assemble i0 e in
+      Ret::i1
+   | StmtBlock(el,_) -> assembleListStmt i0 el
+   | StmtIf(cond,then_,Some(else_),_) ->
+      let i1 = assemble i0 cond in
+      let then_i = Lazy(assemble [] then_) in
+      let else_i = Lazy(assemble [] else_) in
+      Drop::If::else_i::then_i::i1
+   | StmtIf(cond,then_,None,_) ->
+      let i1 = assemble i0 cond in
+      let then_i = Lazy(assemble [] then_) in
+      Drop::If::Value(VUnit)::then_i::i1
+   | StmtFun(name,vars,body,_) ->
+      let i1 = assemble [] body in
+      Store::Reg(name)::Lambda(vars)::Lazy(i1)::i0
+   | StmtWhile(cond,body,_) ->
+      let cond_i = assemble [] cond in
+      let body_i = assemble [] body in
+      Loop::Lazy(body_i)::Lazy(cond_i)::i0
+   | PGroup(e,_) -> assemble i0 e
+   | PUnOp(_,_,_)    -> failwith "No unary operations should remain"
+   | PBinOp(_,_,_,_) -> failwith "No binary operations should remain"
+   | PEmpty          -> failwith "No empty expressions should remain"
+   | StmtEmpty       -> failwith "No empty statements should remain"
+
+and assembleListExp (i0:instruction list) (exp_list:parse_exp list) : instruction list =
+   List.fold_left (fun i e -> assemble i e) i0 exp_list
+and assembleListStmt (i0:instruction list) (exp_list:parse_exp list) : instruction list =
+   List.fold_left (fun i e -> assemble i e) i0 exp_list
+   |> List.rev
+
+let debug (results:parser_results) : unit =
+      let result = CCError.map
+         (fun stmts -> assembleListStmt [] stmts |> printInstructions )
+         results.presult
+      in
+      match result with
+      | `Ok(ret) ->
+         ()
+      | `Error(error) ->
+         ()
 
