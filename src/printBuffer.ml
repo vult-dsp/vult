a1c1322de870f83acf6ea3b1ea6d9fd6559673f1
leonardo <modlfo@gmail.com>
2015-04-09 21:43:40 +0200

    Separates the print buffer

---
+++ src/printBuffer.ml
@@ -0,0 +1,53 @@
+(** Type used to hold a print buffer *)
+type print_buffer =
+   {
+      buffer : Buffer.t;
+      mutable indent : int;
+      mutable space  : string;
+      mutable insert : bool;
+   }
+
+(** Creates a print buffer *)
+let makePrintBuffer () =
+   { buffer = Buffer.create 100; indent = 0; space = ""; insert = false }
+
+(** Inserts a new line to the print buffer *)
+let newline buffer =
+   Buffer.add_string buffer.buffer "\n";
+   buffer.insert <- true
+
+(** Inserts a new line and indents all strings appended *)
+let indent buffer =
+   buffer.indent <- buffer.indent + 1;
+   buffer.space <- String.make (buffer.indent * 3) ' ';
+   newline buffer
+
+(** Removes one indentation step *)
+let outdent buffer =
+   buffer.indent <- buffer.indent - 1;
+   if buffer.indent < 0 then
+      failwith "Cannot outdent more";
+   buffer.space <- String.make (buffer.indent * 3) ' '
+
+(** Inserts a string to the print buffer *)
+let append buffer s =
+   if buffer.insert then
+      begin
+         Buffer.add_string buffer.buffer buffer.space;
+         buffer.insert <- false
+      end;
+   Buffer.add_string buffer.buffer s
+
+(** Returns the contents of the print buffer *)
+let contents buffer =
+   Buffer.contents buffer.buffer
+
+(** Function for printing list of elements *)
+let rec printList buffer f sep l =
+   match l with
+   | []   -> ()
+   | [h]  -> f buffer h
+   | h::t ->
+      f buffer h;
+      append buffer sep;
+      printList buffer f sep t
