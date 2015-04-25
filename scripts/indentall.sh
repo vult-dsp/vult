13afb29e75759cde2d9ccd54b29a22d48fe9a510
leonardo <modlfo@gmail.com>
2015-04-25 10:07:56 +0200

    Updates to include the new sub folders

--- scripts/indentall.sh
+++ scripts/indentall.sh
@@ -5,3 +5,17 @@ do
   cp $file.out $file
   rm $file.out
 done
+
+for file in src/util/*.ml
+do
+  ocp-indent $file > $file.out
+  cp $file.out $file
+  rm $file.out
+done
+
+for file in src/passes/*.ml
+do
+  ocp-indent $file > $file.out
+  cp $file.out $file
+  rm $file.out
+done
