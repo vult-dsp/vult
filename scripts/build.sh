71f53cae9182d8bfcb5b5749c0e5d23614d91efd
leonardo <modlfo@gmail.com>
2015-10-20 19:35:42 +0200

    Fixes build with the latest ppx_deriving

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm vultc.native vultc.d.byte
-ocamlbuild -use-ocamlfind -pkg containers -pkg str -pkg ocamlgraph vultc.native vultc.d.byte
+ocamlbuild -use-ocamlfind -pkg containers -pkg str -pkg ocamlgraph -pkg ppx_deriving  vultc.native vultc.d.byte
