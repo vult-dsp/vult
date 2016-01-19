9a68e2e98d7425fe214f51ad6295df6a0133d7cc
leonardo <modlfo@gmail.com>
2016-01-19 22:49:11 +0100

    Uses vult v0.2

--- javascripts/audio.js
+++ javascripts/audio.js
@@ -17,8 +17,8 @@ var generated_code = "";
 
 function sendNoteOn(note,velocity){
     if(vult_object!=null){
-        if(vult_object.noteOn!=null){
-            vult_object.noteOn(note,velocity);
+        if(vult_object.liveNoteOn!=null){
+            vult_object.liveNoteOn(note,velocity);
         }
     }
     console.log("noteOn("+note+","+velocity+")");
@@ -26,8 +26,8 @@ function sendNoteOn(note,velocity){
 
 function sendNoteOff(note){
     if(vult_object!=null){
-        if(vult_object.noteOff!=null){
-            vult_object.noteOff(note);
+        if(vult_object.liveNoteOff!=null){
+            vult_object.liveNoteOff(note);
         }
     }
     console.log("noteOff("+note+")");
@@ -36,8 +36,8 @@ function sendNoteOff(note){
 function sendControlChange(control,value){
     control_values[control]=value;
     if(vult_object!=null){
-        if(vult_object.controlChange!=null){
-            vult_object.controlChange(control,value);
+        if(vult_object.liveControlChange!=null){
+            vult_object.liveControlChange(control,value);
         }
     }
     console.log("controlChange("+control+","+value+")");
@@ -56,7 +56,8 @@ function updateProgram(){
     var editor = ace.edit("editor");
     var new_vult_object = null;
     try{
-        generated_code = jscode(editor.getValue());
+        generated_code = jscode(editor.getValue()) + " new vultProcess()";
+        //console.log(generated_code);
         new_vult_object = eval(generated_code);
     }
     catch(err) {
@@ -88,8 +89,8 @@ function setAudioOn(){
                 var output = e.outputBuffer.getChannelData(0);
                 for (var i = 0; i < e.inputBuffer.length; i++) {
                     if(vult_object){
-                        if(vult_object.process){
-                            output[i] = vult_object.process(input[i]);
+                        if(vult_object.liveProcess){
+                            output[i] = vult_object.liveProcess(input[i]);
                         }
                     }
                     else{
@@ -127,11 +128,14 @@ function setAudioOff(){
     }
 }
 
+
+
 // All the presets
-var template  = "/* You can use a this template to start a program */\n\n// Main processing function\n// 'input' is by default a sine wave at 440 Hz\nfun process(input){\n   return input;\n}\n\n// Called when a note On is received\nfun noteOn(note,velocity){\n}\n\n// Called when a note Off is received\nfun noteOff(note){\n}\n\n// Called when a control changes\nfun controlChange(control,value){\n}\n\n// Called on initialization to define initial values\nfun default(){\n}\n";
-var volume1   = "/* A simple volume control */\n\n// Used to soften the transitions of controls\nfun smooth(input){\n   mem x;\n   x = x+(input-x)*0.005;\n   return x;\n}\n// Main processing function\n// 'input' is by default a sine wave at 440 Hz\nfun process(input){\n   mem volume; // the value is set in 'controlChange'\n   return input*smooth(volume);\n}\n\n// Called when a note On is received\nfun noteOn(note,velocity){\n}\n\n// Called when a note Off is received\nfun noteOff(note){\n}\n\n// Called when a control changes\nfun controlChange(control,value){\n   mem volume;\n   // Control 30 defines the volume\n   if(control==30) volume = value/127;\n}\n\n// Called on initialization to define initial values\nfun default(){\n mem volume = 0;\n}\n";
-var phasedist = "/* Phase distortion oscillator */\n\n// Used to soften the transitions of controls\nfun smooth(input){\n   mem x;\n   x = x+(input-x)*0.005;\n   return x;\n}\n\n// Returns true every time the input value changes\nfun change(x):bool {\n    mem pre_x;\n    val v:bool = pre_x!=x;\n    pre_x = x;\n    return v;\n}\n\n// Converts the MIDI note to increment rate at a 44100 sample rate\nfun pitchToRate(d) return 8.1758*exp(0.0577623*d)/44100;\n\nfun phasor(pitch,reset){\n    mem rate,phase;\n    if(change(pitch))\n        rate = pitchToRate(pitch);\n    phase = if reset then 0 else (phase + rate) % 1;\n    return phase;\n}\n\n// Main processing function\nfun process(input){\n   mem volume,detune; // values set in 'controlChange'\n   mem pitch;\n   mem pre_phase1;\n   // Implements the resonant filter simulation as shown in\n   // http://en.wikipedia.org/wiki/Phase_distortion_synthesis\n   val phase1 = phasor(pitch,false);\n   val comp   = 1 - phase1;\n   val reset  = (pre_phase1 - phase1) > 0.5;\n   pre_phase1 = phase1;\n   val phase2 = phasor(pitch+smooth(detune)*32,reset);\n   val sine  = sin(2*3.14159265359*phase2);\n   return smooth(volume)*(sine*comp);\n}\n\n// Called when a note On is received\nfun noteOn(note,velocity){\n    mem pitch = note;\n}\n\n// Called when a note Off is received\nfun noteOff(note){\n}\n\n// Called when a control changes\nfun controlChange(control,value){\n   mem volume;\n   mem detune;\n   // Control 30 defines the volume\n   if(control==30) volume = value/127;\n   if(control==31) detune = value/127;\n}\n\n// Called on initialization to define initial values\nfun default(){\n   mem volume = 0;\n   mem pitch = 45;\n   mem detune = 0;\n}\n";
-var synt1     = "/* A simple synthesizer with one LFO */\n\n// Used to soften the transitions of controls\nfun smooth(input){\n   mem x;\n   x = x+(input-x)*0.005;\n   return x;\n}\n\n// Returns true every time the input value changes\nfun change(x):bool {\n    mem pre_x;\n    val v:bool = pre_x!=x;\n    pre_x = x;\n    return v;\n}\n\n// Returns true if the value changes from 0 to anything\nfun edge(x):bool {\n    mem pre_x;\n    val v:bool = (pre_x!=x) && (pre_x==0);\n    pre_x = x;\n    return v;\n}\n\n// Returns true every 'n' calls\nfun each(n){\n   mem count;\n   val ret = (count == 0);\n   count = (count + 1) % n;\n   return ret;\n}\n\n// Converts the MIDI note to increment rate at a 44100 sample rate\nfun pitchToRate(d) return 8.1758*exp(0.0577623*d)/44100;\n\nfun phasor(pitch,reset){\n    mem rate,phase;\n    if(change(pitch))\n        rate = pitchToRate(pitch);\n    phase = if reset then 0 else (phase + rate) % 1;\n    return phase;\n}\n\nfun lfo(f,gate){\n    mem phase;\n    val rate = f * 10/44100;\n    if(edge(gate)) phase = 0;\n    phase = phase + rate;\n    if(phase>1) phase = phase-1;\n    return sin(phase*2*3.141592653589793)-0.5;\n}\n\n// Main processing function\nfun process(input){\n   mem volume,detune; // values set in 'controlChange'\n   mem n1,n2,n3,n4; // used to handle the notes\n   mem count,pitch,gate;\n   mem pre_phase1;\n   mem lfo_rate,lfo_amt;\n   val lfo_val = lfo(lfo_rate,gate)*lfo_amt;\n   // Implements the resonant filter simulation as shown in\n   // http://en.wikipedia.org/wiki/Phase_distortion_synthesis\n   val phase1 = phasor(pitch,false);\n   val comp   = 1 - phase1;\n   val reset  = (pre_phase1 - phase1) > 0.5;\n   pre_phase1 = phase1;\n   val phase2 = phasor(pitch+smooth(detune+lfo_val)*32,reset);\n   val sine  = sin(2*3.14159265359*phase2);\n   return smooth(volume)*(sine*comp)*smooth(gate);\n}\n\n// Called when a note On is received\nfun noteOn(note,velocity){\n   mem n1,n2,n3,n4;\n   mem count,pitch,gate;\n   // written this way because Vult does not have array support yet.\n   if(count == 0) { n1 = note; pitch = note; } else\n   if(count == 1) { n2 = note; pitch = note; } else\n   if(count == 2) { n3 = note; pitch = note; } else\n   if(count == 3) { n4 = note; pitch = note; }\n   if(count <= 4) count = count + 1;\n   gate = if count>0 then 1 else 0;\n}\n\n// Called when a note Off is received\nfun noteOff(note){\n    mem n1,n2,n3,n4;\n    mem count,pitch,gate;\n    val found = false;\n\n    // finds the voice and removes it\n    if(note == n1) { n1,n2,n3 = n2,n3,n4; found = true; } else\n    if(note == n2) { n2,n3    = n3,n4;    found = true; } else\n    if(note == n3) { n3       = n4;       found = true; } else\n    if(note == n4) {                      found = true; }\n\n    // If found, decrease the number of active notes\n    if(found && count>0) count = count - 1;\n\n    gate = if count>0 then 1 else 0;\n\n    if(count == 1) pitch = n1;\n    if(count == 2) pitch = n2;\n    if(count == 3) pitch = n3;\n    if(count == 4) pitch = n4;\n}\n\n// Called when a control changes\nfun controlChange(control,value){\n   mem volume;\n   mem detune;\n   mem lfo_rate;\n   mem lfo_amt;\n   // Control 30 defines the volume\n   if(control==30) volume = value/127;\n   if(control==31) detune = value/127;\n   if(control==32) lfo_rate = value/127;\n   if(control==33) lfo_amt = 2*((value/127)-0.5);\n}\n\n// Called on initialization to define initial values\nfun default(){\n   mem volume = 0;\n   mem pitch = 45;\n   mem detune = 0.8;\n   mem lfo_rate = 0.07;\n   mem lfo_amt = -0.8;\n}\n";
+var template  = "";
+var volume1   = "";
+var phasedist = "";
+var synt1     = "";
+
 
 function loadPreset(n){
     console.log("Loading "+n)
@@ -158,3 +162,20 @@ function loadPreset(n){
     editor.clearSelection();
     editor.gotoLine(0);
 }
+
+var request = new XMLHttpRequest();
+request.onreadystatechange = function() {
+  if (request.readyState != 4) return; // Not there yet
+  if (request.status != 200) {
+    return;
+  }
+  var resp = request.responseText;
+  example_files = JSON.parse(resp);
+  template = example_files.files['template.vult'].content;
+  volume1 = example_files.files['volume.vult'].content;
+  phasedist = example_files.files['phasedist.vult'].content;
+  synt1 = example_files.files['synth1.vult'].content;
+  loadPreset(0);
+}
+request.open('GET', 'https://api.github.com/gists/77bc427f231a1d5b7d8a', true);
+request.send();
