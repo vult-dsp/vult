db0b2b6a92b549e55ce3fcdb727694f35dbe3b4d
leonardo <modlfo@gmail.com>
2016-02-17 00:01:44 +0100

    Updates travis file

--- .travis-vult.sh
+++ .travis-vult.sh
@@ -2,6 +2,6 @@ sh .travis-ocaml.sh
 export OPAMYES=1
 eval $(opam config env)
 
-opam install containers ppx_deriving ounit
+opam install containers ppx_deriving ounit oasis
 ./configure --enable-tests
 make test
