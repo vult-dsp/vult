128e97839335bad283425d4bbbd9ec315a09b0b4
leonardo <modlfo@gmail.com>
2016-02-17 08:45:28 +0100

    Builds the javascript interface in travis-ci

--- .travis-vult.sh
+++ .travis-vult.sh
@@ -2,6 +2,6 @@ sh .travis-ocaml.sh
 export OPAMYES=1
 eval $(opam config env)
 
-opam install containers ppx_deriving ounit oasis
-./configure --enable-tests
+opam install containers ppx_deriving ounit oasis js_of_ocaml
+./configure --enable-tests --enable-js
 make test
