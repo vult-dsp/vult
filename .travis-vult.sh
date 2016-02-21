b8e6e9f99cbcb169fd4efcc8f8f424b4bc9688c4
leonardo <modlfo@gmail.com>
2016-02-21 12:47:47 +0100

    Do not install oasis when building in travis-ci

--- .travis-vult.sh
+++ .travis-vult.sh
@@ -2,6 +2,6 @@ sh .travis-ocaml.sh
 export OPAMYES=1
 eval $(opam config env)
 
-opam install containers ppx_deriving ounit oasis js_of_ocaml
+opam install containers ppx_deriving ounit js_of_ocaml
 ./configure --enable-tests --enable-js
 make test
