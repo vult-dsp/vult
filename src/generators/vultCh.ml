738a1df26887b7da448ca8574e8195c3ae731873
Leonardo <modlfo@gmail.com>
2016-06-16 21:51:37 +0200

    Generates code based on Pla

--- src/generators/vultCh.ml
+++ src/generators/vultCh.ml
@@ -47,26 +47,24 @@ module Templates = struct
 
    let header (name:string) (code:string) : string =
       let file = String.uppercase name in
-      Printf.sprintf
-         "#ifndef %s_H
-#define %s_H
+      Pla.print
+{pla|#ifndef <#file#s>_H
+#define <#file#s>_H
 #include <stdint.h>
 #include <math.h>
-#include \"vultin.h\"
+#include "vultin.h"
 
-%s
+<#code#s>
 
-#endif // %s_H
-"
-         file file code file
+#endif // <#file#s>_H
+|pla}
 
    let implementation (file:string) (code:string) : string =
-      Printf.sprintf
-         "#include \"%s.h\"
+      Pla.print
+{pla|#include "<#file#s>.h"
 
-%s
-"
-         file code
+<#code#s>
+|pla}
 
    let apply template file code =
       match template with
@@ -120,62 +118,45 @@ module PrintC = struct
          int_of_float (n *. (float_of_int 0x10000))
      in Printf.sprintf "0x%x /* %f */" value n*)
 
-   let rec printExp (params:parameters) buffer (e:cexp) : unit =
+   let rec printExp (params:parameters) (e:cexp) : Pla.template =
       match e with
       | CEFloat(n) when params.real = Fixed ->
-         if n < 0.0 then append buffer "(";
-         append buffer (toFixed n);
-         if n < 0.0 then append buffer ")"
-      | CEFloat(n)  ->
-         if n < 0.0 then append buffer "(";
-         append buffer ((string_of_float n)^"f");
-         if n < 0.0 then append buffer ")";
-      | CEInt(n)    ->
-         if n < 0 then append buffer "(";
-         append buffer (string_of_int n);
-         if n < 0 then append buffer ")"
-      | CEBool(v)   -> append buffer (if v then "1" else "0")
-      | CEString(s) -> append buffer ("\"" ^ s ^ "\"")
+         let sn = toFixed n in
+         if n < 0.0 then {pla|(<#sn#s>)|pla} else Pla.string sn
+      | CEFloat(n) ->
+         if n < 0.0 then {pla|(<#n#f>f)|pla} else {pla|<#n#f>f|pla}
+      | CEInt(n) ->
+         if n < 0 then {pla|(<#n#i>)|pla} else {pla|<#n#i>|pla}
+      | CEBool(v)   -> Pla.int (if v then 1 else 0)
+      | CEString(s) -> {pla|"<#s#s>"|pla}
       | CEArray(elems) ->
-         append buffer "{";
-         printList buffer (printExp params) "," elems;
-         append buffer "}"
+         let telems = Pla.map_sep Pla.comma (printExp params) elems in
+         {pla|{<#telems#>}|pla}
       | CECall(name,args) ->
-         append buffer name;
-         append buffer "(";
-         printList buffer (printExp params) "," args;
-         append buffer ")"
+         let targs = Pla.map_sep Pla.comma (printExp params) args in
+         {pla|<#name#s>(<#targs#>)|pla}
       | CEUnOp(op,e) ->
-         append buffer "(";
-         append buffer op;
-         append buffer " ";
-         printExp params buffer e;
-         append buffer ")"
+         let te = printExp params e in
+         {pla|(<#op#s> <#te#>)|pla}
       | CEOp(op,elems) ->
-         append buffer "(";
-         printList buffer (printExp params) (" "^(op)^" ") elems;
-         append buffer ")";
+         let sop = {pla| <#op#s> |pla} in
+         let telems = Pla.map_sep sop (printExp params) elems in
+         {pla|(<#telems#>)|pla}
       | CEVar(name) ->
-         append buffer name
+         Pla.string name
       | CEIf(cond,then_,else_) ->
-         append buffer "(";
-         printExp params buffer cond;
-         append buffer "?";
-         printExp params buffer then_;
-         append buffer ":";
-         printExp params buffer else_;
-         append buffer ")"
-      | CENewObj -> append buffer "{}"
+         let tcond = printExp params cond in
+         let tthen = printExp params then_ in
+         let telse = printExp params else_ in
+         {pla|(<#tcond#>?<#tthen#>:<#telse#>)|pla}
+      | CENewObj -> Pla.string "{}"
       | CETuple(elems) ->
-         append buffer "{ ";
-         printList buffer (printChField params) ", " elems;
-         append buffer " }"
+         let telems = Pla.map_sep Pla.comma (printChField params) elems in
+         {pla|{ <#telems#> }|pla}
 
-   and printChField params buffer (name,value) =
-      append buffer ".";
-      append buffer name;
-      append buffer " = ";
-      printExp params buffer value
+   and printChField params (name,value) =
+      let tval = printExp params value in
+      {pla|.<#name#s> = <#tval#>|pla}
 
    let isSpecial name =
       match name with
@@ -193,240 +174,164 @@ module PrintC = struct
          let name,sub_size = simplifyArray sub in
          name, sub_size @ [string_of_int size]
 
-   let printTypeDescr buffer (typ:type_descr) =
+   let printTypeDescr (typ:type_descr) : Pla.template =
       let kind, sizes = simplifyArray typ in
       match sizes with
-      | [] ->
-         append buffer kind
+      | [] -> Pla.string kind
       | _ ->
-         append buffer kind;
-         append buffer "[";
-         printList buffer append ", " sizes;
-         append buffer "]"
+         let tsize = Pla.map_sep Pla.comma Pla.string sizes in
+         {pla|<#kind#s>[<#tsize#>]|pla}
 
-   let printTypeAndName (is_decl:bool) buffer (typ:type_descr) (name:string) =
+   let printTypeAndName (is_decl:bool) (typ:type_descr) (name:string) : Pla.template =
       let kind, sizes = simplifyArray typ in
-      if is_decl then (append buffer kind; append buffer " ");
-      match sizes with
-      | [] ->
-         append buffer name
-      | _ ->
-         append buffer name;
-         if is_decl then begin
-            append buffer "[";
-            printList buffer append ", " sizes;
-            append buffer "]"
-         end
+      match is_decl, sizes with
+      | true,[] -> {pla|<#kind#s> <#name#s>|pla}
+      | true,_  ->
+            let t_sizes = Pla.map_sep Pla.comma Pla.string sizes in
+            {pla|<#kind#s> <#name#s>[<#t_sizes#>]|pla}
+      | _,_ -> {pla|<#name#s>|pla}
 
-   let printLhsExpTuple buffer (var:string) (is_var:bool) (i:int) (e:clhsexp) : unit =
+   let printLhsExpTuple (var:string) (is_var:bool) (i:int) (e:clhsexp) : Pla.template =
       match e with
       | CLId(CTSimple(typ),name) ->
-         if is_var then (append buffer typ; append buffer " ");
-         append buffer name;
-         append buffer " = ";
-         append buffer var;
-         append buffer (".field_"^(string_of_int i));
-         append buffer "; "
+         if is_var then
+            {pla|<#typ#s> <#name#s> = <#var#s>.field_<#i#i>;|pla}
+         else
+            {pla|<#name#s> = <#var#s>.field_<#i#i>;|pla}
       | CLId(typ,name) ->
-         printTypeAndName is_var buffer typ name;
-         append buffer " = ";
-         append buffer var;
-         append buffer (".field_"^(string_of_int i));
-         append buffer "; "
-      | CLWild -> ()
+         let tdecl = printTypeAndName is_var typ name in
+         {pla|<#tdecl#> = <#var#s>.field_<#i#i>;|pla}
+      | CLWild -> Pla.unit
 
       | _ -> failwith "printLhsExpTuple: All other cases should be already covered"
 
-   let printArrayBinding params buffer (var:string) (i:int) (e:cexp) : unit =
-      append buffer var;
-      append buffer "[";
-      append buffer (string_of_int i);
-      append buffer "] = ";
-      printExp params buffer e;
-      append buffer "; "
+   let printArrayBinding params (var:string) (i:int) (e:cexp) : Pla.template =
+      let te = printExp params e in
+      {pla|<#var#s>[<#i#i>] = <#te#>; |pla}
 
-   let printLhsExp buffer (is_var:bool) (e:clhsexp) : unit =
+   let printLhsExp (is_var:bool) (e:clhsexp) : Pla.template =
       match e with
-      | CLId(CTSimple(typ),name) ->
-         if is_var then (append buffer typ; append buffer " ");
-         append buffer name;
+      | CLId(CTSimple(typ),name) when is_var ->
+         {pla|<#typ#s> <#name#s>|pla}
+      | CLId(CTSimple(_),name) ->
+         Pla.string name
       | CLId(typ,name) ->
-         printTypeAndName is_var buffer typ name
-      | CLWild -> ()
+         printTypeAndName is_var typ name
+      | CLWild -> Pla.unit
 
       | _ -> failwith "printLhsExp: All other cases should be already covered"
 
-   let printFunArg buffer (ntype,name) =
+   let printFunArg (ntype,name) : Pla.template =
       match ntype with
       | Var(typ) ->
-         printTypeDescr buffer typ;
-         append buffer " ";
-         append buffer name
+         let tdescr = printTypeDescr typ in
+         {pla|<#tdescr#> <#name#s>|pla}
       | Ref(typ) ->
-         printTypeDescr buffer typ;
-         append buffer " &";
-         append buffer name
+         let tdescr = printTypeDescr typ in
+         {pla|<#tdescr#> &<#name#s>|pla}
 
-   let rec printStmt (params:parameters) buffer (stmt:cstmt) : bool =
+   let rec printStmt (params:parameters) (stmt:cstmt) : Pla.template option =
       match stmt with
-      | CSVarDecl(CLWild,None) -> false
+      | CSVarDecl(CLWild,None) -> None
       | CSVarDecl(CLWild,Some(value)) ->
-         printExp params buffer value;
-         append buffer ";";
-         true
+         let te = printExp params value in
+         Some({pla|<#te#>;|pla})
       | CSVarDecl((CLId(_,_) as lhs),Some(value)) ->
-         printLhsExp buffer true lhs;
-         append buffer " = ";
-         printExp params buffer value;
-         append buffer ";";
-         true
+         let tlhs = printLhsExp true lhs in
+         let te   = printExp params value in
+         Some({pla|<#tlhs#> = <#te#>;|pla})
       | CSVarDecl((CLId(_,_) as lhs),None) ->
-         printLhsExp buffer true lhs;
-         append buffer ";";
-         true
+         let tlhs = printLhsExp true lhs in
+         Some({pla|<#tlhs#>;|pla})
       | CSVarDecl(CLTuple(elems),Some(CEVar(name))) ->
-         List.iteri (printLhsExpTuple buffer name true) elems;
-         true
+         let t = List.mapi (printLhsExpTuple name true) elems |> Pla.join in
+         Some(t)
       | CSVarDecl(CLTuple(_),_) -> failwith "printStmt: invalid tuple assign"
       | CSBind(CLWild,value) ->
-         printExp params buffer value;
-         append buffer ";";
-         true
+         let te = printExp params value in
+         Some({pla|<#te#>;|pla})
       | CSBind(CLTuple(elems),CEVar(name)) ->
-         List.iteri (printLhsExpTuple buffer name false) elems;
-         true
+         let t =List.mapi (printLhsExpTuple name false) elems |> Pla.join in
+         Some(t)
       | CSBind(CLTuple(_),_) -> failwith "printStmt: invalid tuple assign"
       | CSBind(CLId(_,name),CEArray(elems)) ->
-         List.iteri (printArrayBinding params buffer name) elems;
-         true
+         let t = List.mapi (printArrayBinding params name) elems |> Pla.join in
+         Some(t)
       | CSBind(CLId(_,name),value) ->
-         append buffer name;
-         append buffer " = ";
-         printExp params buffer value;
-         append buffer ";";
-         true
+         let te = printExp params value in
+         Some({pla|<#name#s> = <#te#>;|pla})
       | CSFunction(ntype,name,args,(CSBlock(_) as body)) ->
-         printTypeDescr buffer ntype;
-         append buffer " ";
-         append buffer name;
-         append buffer "(";
-         printList buffer printFunArg ", " args;
-         append buffer ")";
+         let ret = printTypeDescr ntype in
+         let targs = Pla.map_sep Pla.commaspace printFunArg args in
          if params.is_header then begin
-            append buffer ";";
-            newline buffer;
-            true
+            Some({pla|<#ret#> <#name#s>(<#targs#>);|pla})
          end
          else begin
-            printStmt params buffer body |> ignore;
-            newline buffer;
-            true
+            match printStmt params body with
+            | Some(tbody) ->
+                  Some({pla|<#ret#> <#name#s>(<#targs#>)<#tbody#>|pla})
+            | None -> Some({pla|<#ret#> <#name#s>(<#targs#>);|pla})
          end
       | CSFunction(ntype,name,args,body) ->
-         printTypeDescr buffer ntype;
-         append buffer " ";
-         append buffer name;
-         append buffer "(";
-         printList buffer printFunArg ", " args;
-         append buffer ")";
-         if params.is_header then begin
-            append buffer ";";
-            newline buffer;
-            true
-         end
-         else begin
-            append buffer "{ ";
-            printStmt params buffer body |> ignore;
-            append buffer "}";
-            newline buffer;
-            true
-         end
+         let ret = printTypeDescr ntype in
+         let targs = Pla.map_sep Pla.commaspace printFunArg args in
+         if params.is_header then
+            Some({pla|<#ret#> <#name#s>(<#targs#>);|pla})
+         else
+            let tbody = CCOpt.get_or ~default:Pla.semi (printStmt params body) in
+            Some({pla|<#ret#> <#name#s>(<#targs#>){ <#tbody#>}|pla})
       | CSReturn(e1) ->
-         append buffer "return ";
-         printExp params buffer e1;
-         append buffer ";";
-         true
+         let te = printExp params e1 in
+         Some({pla|return <#te#>;|pla})
       | CSWhile(cond,body) ->
-         append buffer "while(";
-         printExp params buffer cond;
-         append buffer ")";
-         printStmt params buffer body;
+         let tcond = printExp params cond in
+         let tbody = CCOpt.get_or ~default:Pla.semi (printStmt params body) in
+         Some({pla|while(<#tcond#>)<#tbody#>|pla})
       | CSBlock(elems) ->
-         append buffer "{";
-         indent buffer;
-         printStmtList buffer params elems;
-         outdent buffer;
-         append buffer "}";
-         true
+         let telems = printStmtList params elems in
+         Some({pla|{<#telems#+>}|pla})
       | CSIf(cond,then_,None) ->
-         append buffer "if";
-         if isSimple cond then append buffer "(";
-         printExp params buffer cond;
-         if isSimple cond then append buffer ")";
-         printStmt params buffer then_;
+         let tcond = printExp params cond in
+         let tcond = if isSimple cond then Pla.wrap (Pla.string "(") (Pla.string ")") tcond else tcond in
+         let tthen = CCOpt.get_or ~default:Pla.semi (printStmt params then_) in
+         Some({pla|if<#tcond#><#tthen#>|pla})
       | CSIf(cond,then_,Some(else_)) ->
-         append buffer "if";
-         if isSimple cond then append buffer "(";
-         printExp params buffer cond;
-         if isSimple cond then append buffer ")";
-         printStmt params buffer then_ |> ignore;
-         newline buffer;
-         append buffer "else";
-         newline buffer;
-         printStmt params buffer else_ |> ignore;
-         true
+         let tcond = printExp params cond in
+         let tcond = if isSimple cond then Pla.wrap (Pla.string "(") (Pla.string ")") tcond else tcond in
+         let tthen = CCOpt.get_or ~default:Pla.semi (printStmt params then_) in
+         let telse = CCOpt.get_or ~default:Pla.semi (printStmt params else_) in
+         Some({pla|if<#tcond#><#tthen#>
+else
+<#telse#>|pla})
       | CSType(name,members) when params.is_header ->
-         append buffer "typedef struct ";
-         append buffer name;
-         append buffer " {";
-         indent buffer;
-         List.iter
+         let tmembers =
+         Pla.map_sep_all Pla.newline
             (fun (typ, name) ->
-                printTypeAndName true buffer typ name;
-                append buffer ";";
-                newline buffer;
+                let tmember = printTypeAndName true typ name in
+                {pla|<#tmember#>;|pla}
             ) members;
-         outdent buffer;
-         append buffer "} ";
-         append buffer name;
-         append buffer ";";
-         newline buffer;
-         true
-      | CSType(_,_) -> false
+         in
+         Some({pla|typedef struct <#name#s> {<#tmembers#+>} <#name#s>;|pla})
+      | CSType(_,_) -> None
       | CSAlias(t1,t2) when params.is_header ->
-         append buffer "typedef ";
-         append buffer t1;
-         append buffer " ";
-         printTypeDescr buffer t2;
-         append buffer ";";
-         newline buffer;
-         true
-      | CSAlias(_,_) -> false
+         let tdescr = printTypeDescr t2 in
+         Some({pla|typedef <#t1#s> <#tdescr#>;|pla})
+      | CSAlias(_,_) -> None
       | CSExtFunc(ntype,name,args) when params.is_header ->
-         printTypeDescr buffer ntype;
-         append buffer " ";
-         append buffer name;
-         append buffer "(";
-         printList buffer printFunArg ", " args;
-         append buffer ")";
-         append buffer ";";
-         newline buffer;
-         true
-      | CSExtFunc _ -> false
-      | CSEmpty -> false
+         let ret = printTypeDescr ntype in
+         let targs = Pla.map_sep Pla.commaspace printFunArg args in
+         Some({pla|<#ret#> <#name#s>(<#targs#>);|pla})
+      | CSExtFunc _ -> None
+      | CSEmpty -> None
 
-   and printStmtList buffer (params:parameters) (stmts:cstmt list) : unit =
-      match stmts with
-      | [] -> ()
-      | h :: t ->
-         let insert_new_line = printStmt params buffer h in
-         if insert_new_line then newline buffer;
-         printStmtList buffer params t
+   and printStmtList (params:parameters) (stmts:cstmt list) : Pla.template =
+      let tstmts = CCList.filter_map (printStmt params) stmts in
+      Pla.map_sep_all Pla.newline (fun a -> a) tstmts
+
 
    let printChCode (params:parameters) (stmts:cstmt list) : string =
-      let buffer = makePrintBuffer () in
-      let _      = printStmtList buffer params stmts in
-      let code   = contents buffer in
+      let t     = printStmtList params stmts in
+      let code  = Pla.print t in
       Templates.apply params.template params.output code
 
 end
