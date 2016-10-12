18331df21e4163329e9c4089b093f7834fb90892
Leonardo <modlfo@gmail.com>
2016-10-12 23:24:41 +0300

    Tries rebuilding the project since seems like the script uninstall the files

--- scripts/makezip.sh
+++ scripts/makezip.sh
@@ -1,1 +1,4 @@
-7z a $APPVEYOR_BUILD_FOLDER/vult.zip `opam config var bin`/vultc runtime/vultin.c runtime/vultin.h
+cd $APPVEYOR_BUILD_FOLDER
+ocaml setup.ml -build
+ocaml setup.ml -install
+7z a $APPVEYOR_BUILD_FOLDER/vult.zip `opam config var bin`/vultc $APPVEYOR_BUILD_FOLDER/runtime/vultin.c $APPVEYOR_BUILD_FOLDER/runtime/vultin.h
