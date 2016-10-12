b9c53898b82849b05d1e07af0525efe2fe90d2ae
Leonardo <modlfo@gmail.com>
2016-10-12 23:36:50 +0300

    When makeing the zip file recompiles the project without installing it

--- scripts/makezip.sh
+++ scripts/makezip.sh
@@ -1,4 +1,5 @@
 cd $APPVEYOR_BUILD_FOLDER
+ocaml setup.ml -configure
 ocaml setup.ml -build
-ocaml setup.ml -install
-7z a $APPVEYOR_BUILD_FOLDER/vult.zip `opam config var bin`/vultc $APPVEYOR_BUILD_FOLDER/runtime/vultin.c $APPVEYOR_BUILD_FOLDER/runtime/vultin.h
+cp vultc.native vultc.exe
+7z a $APPVEYOR_BUILD_FOLDER/vult.zip $APPVEYOR_BUILD_FOLDER/vultc.exe $APPVEYOR_BUILD_FOLDER/runtime/vultin.c $APPVEYOR_BUILD_FOLDER/runtime/vultin.h
