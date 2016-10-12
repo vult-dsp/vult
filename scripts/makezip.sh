4acca0895c5b8114c3b9d63abe20ff5db17cd46b
Leonardo <modlfo@gmail.com>
2016-10-12 23:45:23 +0300

    Fixes an error when specifiying the file

--- scripts/makezip.sh
+++ scripts/makezip.sh
@@ -1,5 +1,5 @@
 cd $APPVEYOR_BUILD_FOLDER
 ocaml setup.ml -configure
 ocaml setup.ml -build
-cp vultc.native vultc.exe
-7z a $APPVEYOR_BUILD_FOLDER/_build/vult.zip $APPVEYOR_BUILD_FOLDER/vultc.exe $APPVEYOR_BUILD_FOLDER/runtime/vultin.c $APPVEYOR_BUILD_FOLDER/runtime/vultin.h
+cp $APPVEYOR_BUILD_FOLDER/_build/vultc.native $APPVEYOR_BUILD_FOLDER/vultc.exe
+7z a $APPVEYOR_BUILD_FOLDER/vult.zip $APPVEYOR_BUILD_FOLDER/vultc.exe $APPVEYOR_BUILD_FOLDER/runtime/vultin.c $APPVEYOR_BUILD_FOLDER/runtime/vultin.h
