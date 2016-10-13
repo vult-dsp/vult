4f4b25f2cf2d6e6f9adf703ebf4f5bfad09f55ab
Leonardo <modlfo@gmail.com>
2016-10-13 10:08:15 +0300

    Updates the appveyor release settings

--- scripts/makezip.sh
+++ scripts/makezip.sh
@@ -2,4 +2,4 @@ cd $APPVEYOR_BUILD_FOLDER
 ocaml setup.ml -configure
 ocaml setup.ml -build
 cp $APPVEYOR_BUILD_FOLDER/_build/src/vultc.native $APPVEYOR_BUILD_FOLDER/vultc.exe
-7z a $APPVEYOR_BUILD_FOLDER/vult.zip $APPVEYOR_BUILD_FOLDER/vultc.exe $APPVEYOR_BUILD_FOLDER/runtime/vultin.c $APPVEYOR_BUILD_FOLDER/runtime/vultin.h
+7z a $APPVEYOR_BUILD_FOLDER/vult-v0.3-win.zip $APPVEYOR_BUILD_FOLDER/vultc.exe $APPVEYOR_BUILD_FOLDER/runtime/vultin.c $APPVEYOR_BUILD_FOLDER/runtime/vultin.h
