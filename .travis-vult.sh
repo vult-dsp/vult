1ed7abaf354e1cac8ab60210a60cd9786fa29b16
Leonardo <modlfo@gmail.com>
2016-10-13 22:07:07 +0300

    Tries to create a package after building vult with travis

--- .travis-vult.sh
+++ .travis-vult.sh
@@ -5,3 +5,9 @@ eval $(opam config env)
 opam install containers ppx_deriving ounit js_of_ocaml pla
 ./configure --enable-tests --enable-js
 make test
+cp ./_build/src/vultc.native ./vultc
+if [ $TRAVIS_OS_NAME == linux ] then
+   tar -cvzf vult-v0.3-linux.tar.gz vultc runtime/vultin.h vultin.c
+else
+   tar -cvzf vult-v0.3-osx.tar.gz vultc runtime/vultin.h vultin.c
+fi
