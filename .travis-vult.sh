52f40ad241f07b6b7efe1d41e3a580471f171ba1
Leonardo <modlfo@gmail.com>
2016-10-13 22:34:09 +0300

    Fixes small problem in the compress script

--- .travis-vult.sh
+++ .travis-vult.sh
@@ -6,8 +6,9 @@ opam install containers ppx_deriving ounit js_of_ocaml pla
 ./configure --enable-tests --enable-js
 make test
 cp ./_build/src/vultc.native ./vultc
-if [ $TRAVIS_OS_NAME == linux ] then
-   tar -cvzf vult-v0.3-linux.tar.gz vultc runtime/vultin.h vultin.c
+if [ $TRAVIS_OS_NAME == linux ];
+then
+   tar -cvzf vult-v0.3-linux.tar.gz vultc runtime/vultin.h runtime/vultin.c
 else
-   tar -cvzf vult-v0.3-osx.tar.gz vultc runtime/vultin.h vultin.c
+   tar -cvzf vult-v0.3-osx.tar.gz vultc runtime/vultin.h runtime/vultin.c
 fi
