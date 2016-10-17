db348d518eae3768e9ce900c6528e10136c344c4
Leonardo <modlfo@gmail.com>
2016-10-17 18:42:05 +0300

    Test installation with npm in appveyor

--- scripts/makezip.sh
+++ scripts/makezip.sh
@@ -3,3 +3,5 @@ ocaml setup.ml -configure
 ocaml setup.ml -build
 cp $APPVEYOR_BUILD_FOLDER/_build/src/vultc.native $APPVEYOR_BUILD_FOLDER/vultc.exe
 7z a $APPVEYOR_BUILD_FOLDER/vult-win.zip $APPVEYOR_BUILD_FOLDER/vultc.exe $APPVEYOR_BUILD_FOLDER/runtime/vultin.c $APPVEYOR_BUILD_FOLDER/runtime/vultin.h
+npm install vult -g
+vultc --help
