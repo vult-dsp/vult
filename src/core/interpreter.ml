39c5acf5c040a4315d7612f6a3c4f0f15d17f1dc
Leonardo <modlfo@gmail.com>
2016-11-13 18:03:01 +0100

    Adds builtin functions to the interpreter

--- src/core/interpreter.ml
+++ src/core/interpreter.ml
@@ -28,12 +28,16 @@ open Common
 
 module Env = struct
 
+   type fun_body =
+      | Declared of stmt
+      | Builtin of (exp list -> exp)
+
    type t =
       {
          locals    : (id, exp) Hashtbl.t list;
          context   : (id, exp) Hashtbl.t;
          instances : (id, t) Hashtbl.t;
-         functions : (id, stmt) Hashtbl.t;
+         functions : (id, fun_body) Hashtbl.t;
          modules   : (id, t) Hashtbl.t;
       }
 
@@ -56,9 +60,9 @@ module Env = struct
       | t ::_ -> t
       | []   -> failwith "invalid env"
 
-   let rec lookupFunction_loop (t:t) (env:env) (id:id) : stmt =
+   let rec lookupFunction_loop (t:t) (env:env) (id:id) : fun_body =
       match id with
-      | [] -> failwith "unknown function"
+      | [] -> failwith ("unknown function: "^(PrintTypes.identifierStr id))
       | name1::name2 ->
          if Hashtbl.mem t.functions [name1] && name2 = [] then
             (* exists in the functions table and it's a single name *)
@@ -71,9 +75,9 @@ module Env = struct
             (* not found, go one level up *)
             match env with
             | t' :: env' -> lookupFunction_loop t' env' id
-            | [] -> failwith "unknown function"
+            | [] -> failwith ("unknown function: "^(PrintTypes.identifierStr id))
 
-   let lookupFunction (env:env) (id:id) : stmt =
+   let lookupFunction (env:env) (id:id) : fun_body =
       match env with
       | h::t -> lookupFunction_loop h t id
       | []   -> failwith "invalid env"
@@ -112,7 +116,7 @@ module Env = struct
 
    let rec updateVar_loop (locals:'a list) (id:id) (value:exp) : unit =
       match locals with
-      | [] -> failwith "unknow variable"
+      | [] -> failwith ("unknow variable: "^(PrintTypes.identifierStr id))
       | h::t ->
          match Hashtbl.mem h id with
          | _ -> Hashtbl.replace h id value
@@ -138,7 +142,7 @@ module Env = struct
       let t = first env in
       match Hashtbl.find t.modules id with
       | module_t -> module_t :: env
-      | exception Not_found -> failwith "unknown module"
+      | exception Not_found -> failwith ("unknown module: "^(PrintTypes.identifierStr id))
 
    (** Inserts a table to store local variables *)
    let enterLocal (env:env) : env =
@@ -171,6 +175,11 @@ let makeInstName (fn:id) (attr:attr) : id =
    match fn with
    | [id] -> [id^"_"^line^"_"^col]
    | _ -> failwith "invalid function name"
+
+let isTrue (cond:exp) : bool =
+   match cond with
+   | PBool(b, _) -> b
+   | _ -> failwith "could not evaluate condition"
 
 (** Returns the initial value given an expression *)
 let rec getInitValue (tp:VType.t) : exp =
@@ -226,7 +235,66 @@ let evalOp (op:string) (e1:exp) (e2:exp) : exp =
    | ">=", PInt(v1, attr),  PInt(v2, _)  -> PBool(v1 >= v2, attr)
    | "<=", PReal(v1, attr), PReal(v2, _) -> PBool(v1 <= v2, attr)
    | "<=", PInt(v1, attr),  PInt(v2, _)  -> PBool(v1 <= v2, attr)
+   | "&&", PBool(v1, attr),  PBool(v2, _)  -> PBool(v1 && v2, attr)
+   | "||", PBool(v1, attr),  PBool(v2, _)  -> PBool(v1 || v2, attr)
    | _ -> POp(op, [e1; e2], emptyAttr)
+
+let builtinFunctions env =
+   let real_real f args : exp =
+      match args with
+      | [PReal(v,attr)] -> PReal(f v, attr)
+      | _ -> failwith "invalid arguments"
+   in
+   let clip args =
+      match args with
+      | [PReal(v,attr); PReal(mi,_); PReal(ma,_)] -> PReal(max mi (min ma v) ,attr)
+      | [PInt(v,attr); PInt(mi,_); PInt(ma,_)] -> PInt(max mi (min ma v) ,attr)
+      | _ -> failwith "clip: invalid arguments"
+   in
+   let int args =
+      match args with
+      | [PReal(v,attr)] -> PInt(int_of_float v,attr)
+      | [PInt(v,attr)]  -> PInt(v,attr)
+      | [PBool(v,attr)] -> PInt((if v then 1 else 0),attr)
+      | _ -> failwith "int: invalid arguments"
+   in
+   let float args =
+      match args with
+      | [PReal(v,attr)] -> PReal(v,attr)
+      | [PInt(v,attr)]  -> PReal(float_of_int v,attr)
+      | [PBool(v,attr)] -> PReal((if v then 1.0 else 0.0),attr)
+      | _ -> failwith "real: invalid arguments"
+   in
+   let not args =
+      match args with
+      | [PBool(v,attr)] -> PBool(not v,attr)
+      | _ -> failwith "real: invalid arguments"
+   in
+   let eps args =
+      match args with
+      | [] -> PReal(1e-18,emptyAttr)
+      | _ -> failwith "eps: invalid arguments"
+   in
+   let functions =
+      [
+         "abs", Env.Builtin(real_real abs_float);
+         "exp", Env.Builtin(real_real exp);
+         "sin", Env.Builtin(real_real sin);
+         "cos", Env.Builtin(real_real cos);
+         "floor", Env.Builtin(real_real floor);
+         "tanh", Env.Builtin(real_real tanh);
+         "sqrt", Env.Builtin(real_real sqrt);
+         "clip", Env.Builtin(clip);
+
+         "int", Env.Builtin(int);
+         "float", Env.Builtin(float);
+
+         "not", Env.Builtin(not);
+         "eps", Env.Builtin(eps);
+
+      ]
+   in
+   List.iter (fun (name,body) ->Env.addFunction env [name] body) functions
 
 let foldOp (op:string) (args:exp list) : exp =
    match args with
@@ -239,21 +307,28 @@ type bind_kind =
    | DeclareVal
    | DeclareMem
 
-let rec bind (kind:bind_kind) (env:Env.env) (lhs:lhs_exp) (rhs:exp) =
-   match lhs, rhs with
-   | LWild _, _ -> ()
-   | LTyped(lhs,_,_),_
-   | LGroup(lhs,_),_ ->
-      bind kind env lhs rhs
-   | LId(id, _, _), rhs ->
-      begin
-         match kind with
-         | Update     -> Env.updateVar env id rhs
-         | DeclareVal -> Env.declareVal env id rhs
-         | DeclareMem -> Env.declareMem env id rhs
-      end
-   | LTuple(lhs_elems,_), PTuple(rhs_elems,_) ->
-      List.iter2 (bind kind env) lhs_elems rhs_elems
+let rec bind (kind:bind_kind) (env:Env.env) (lhs:lhs_exp) (rhs:exp option) =
+   match lhs, rhs, kind with
+   | LWild _, _, _ -> ()
+   | LTyped(lhs,_,_),_,_
+   | LGroup(lhs,_),_,_ -> bind kind env lhs rhs
+   | LId(id, _, _), Some(rhs), Update     -> Env.updateVar env id rhs
+   | LId(id, _, _), Some(rhs), DeclareVal -> Env.declareVal env id rhs
+   | LId(id, _, _), Some(rhs), DeclareMem -> Env.declareMem env id rhs
+   | LId(id, _, _), None, Update ->
+      let rhs = getInitExp lhs in
+      Env.updateVar env id rhs
+   | LId(id, _, _), None, DeclareVal ->
+      let rhs = getInitExp lhs in
+      Env.declareVal env id rhs
+   | LId(id, _, _), None, DeclareMem ->
+      let rhs = getInitExp lhs in
+      Env.declareMem env id rhs
+   | LTuple(lhs_elems,_), Some(PTuple(rhs_elems,_)),_ ->
+      List.iter2 (fun l r -> bind kind env l (Some(r))) lhs_elems rhs_elems
+   | LTuple(lhs_elems,_), None,_ ->
+      List.iter (fun l -> bind kind env l None) lhs_elems
+
    | _ -> failwith "Interpreter.bind: invalid input"
 
 let rec bind_arg (env:Env.env) (lhs:typed_id) (rhs:exp) =
@@ -269,7 +344,12 @@ let rec evalExp (env:Env.env) (exp:exp) : exp =
    | PInt _  -> exp
    | PReal _ -> exp
    | PGroup(e, _) -> evalExp env e
-   | PId(id,_) ->  Env.lookupVar env id
+   | PId(id,_) ->
+      begin
+         match Env.lookupVar env id with
+         | value -> value
+         | exception Not_found -> exp
+      end
    | PUnOp(op, exp, _) ->
       let exp' = evalExp env exp in
       evalUop op exp'
@@ -312,38 +392,37 @@ let rec evalExp (env:Env.env) (exp:exp) : exp =
       let env'  = Env.enterInstance env inst in
       evalFunction env' fn args'
 
-and evalFunction (env:Env.env) (fn:stmt) (args:exp list) : exp =
+and evalFunction (env:Env.env) (fn:Env.fun_body) (args:exp list) : exp =
    match fn with
-   | StmtFun(_, inputs, stmt, _, _) ->
-      List.iter2 (bind_arg env) inputs args ;
+   | Env.Declared(StmtFun(_, inputs, stmt, _, _)) ->
+      List.iter2 (bind_arg env) inputs args;
       evalStmt env stmt
+   | Env.Builtin(fn) -> fn args
    | _ -> failwith "cannot evaluate function"
 
 and evalStmt (env:Env.env) (stmt:stmt) =
    match stmt with
    | StmtVal(lhs, Some(rhs), _) ->
       let rhs' = evalExp env rhs in
-      bind Update env lhs rhs';
+      bind Update env lhs (Some(rhs'));
       ret_unit
 
    | StmtVal(lhs, None, _) ->
-      let rhs = getInitExp lhs in
-      bind DeclareVal env lhs rhs;
+      bind DeclareVal env lhs None;
       ret_unit
 
    | StmtMem(lhs, Some(rhs), _) ->
       let rhs' = evalExp env rhs in
-      bind Update env lhs rhs';
+      bind Update env lhs (Some(rhs'));
       ret_unit
 
    | StmtMem(lhs, None, _) ->
-      let rhs = getInitExp lhs in
-      bind DeclareMem env lhs rhs;
+      bind DeclareMem env lhs None;
       ret_unit
 
    | StmtBind(lhs, rhs, _) ->
       let rhs' = evalExp env rhs in
-      bind Update env lhs rhs';
+      bind Update env lhs (Some(rhs'));
       ret_unit
 
    | StmtReturn(e, _) -> evalExp env e
@@ -353,10 +432,51 @@ and evalStmt (env:Env.env) (stmt:stmt) =
       evalStmts env' stmts
 
    | StmtFun(name,_,_,_,_) ->
-      Env.addFunction env name stmt;
+      Env.addFunction env name (Env.Declared(stmt));
       ret_unit
 
-   | h -> failwith ("Not implemented "^(show_stmt h))
+   | StmtEmpty -> ret_unit
+
+   | StmtIf(cond, then_, None, _) ->
+      let cond' = evalExp env cond in
+      begin
+         match cond' with
+         | PBool(true, _) ->
+            evalStmt env then_
+         | PBool(false, _) ->
+            ret_unit
+         | _ -> failwith "could not evaluate if statement"
+      end
+   | StmtIf(cond, then_, Some(else_), _) ->
+      let cond' = evalExp env cond in
+      begin
+         match cond' with
+         | PBool(true, _) ->
+            evalStmt env then_
+         | PBool(false, _) ->
+            evalStmt env else_
+         | _ -> failwith "could not evaluate if statement"
+      end
+   | StmtWhile(cond,body,_) ->
+      let rec loop () =
+         let cond' = evalExp env cond in
+         match cond' with
+         | PBool(true, _) ->
+            let ret = evalStmt env body in
+            begin
+               match ret with
+               | PUnit _ -> loop ()
+               | _ -> ret
+            end
+         | PBool(false, _) -> ret_unit
+         | _ -> failwith "condition cannot be evaluated"
+      in
+      loop ()
+
+   | StmtType _ -> ret_unit
+   | StmtAliasType _ -> ret_unit
+   | StmtExternal _ -> ret_unit
+
 
 and evalStmts (env:Env.env) (stmts:stmt list) : exp =
    match stmts with
@@ -377,4 +497,5 @@ let evalModule (env:Env.env) results =
 
 let eval (results:parser_results list) =
    let env = Env.top () in
+   builtinFunctions env;
    List.map  (evalModule env) results
