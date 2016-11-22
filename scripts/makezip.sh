ed52455135cbf05ec4be3c00833fae424b4cae92
Leonardo <modlfo@gmail.com>
2016-11-22 21:34:08 +0100

    Packages the dll in windows

--- scripts/makezip.sh
+++ scripts/makezip.sh
@@ -1,7 +1,6 @@
 cd $APPVEYOR_BUILD_FOLDER
-ocaml setup.ml -configure
-ocaml setup.ml -build
 cp $APPVEYOR_BUILD_FOLDER/_build/src/vultc.native $APPVEYOR_BUILD_FOLDER/vultc.exe
-7z a $APPVEYOR_BUILD_FOLDER/vult-win.zip $APPVEYOR_BUILD_FOLDER/vultc.exe $APPVEYOR_BUILD_FOLDER/runtime/vultin.c $APPVEYOR_BUILD_FOLDER/runtime/vultin.h
+7z a $APPVEYOR_BUILD_FOLDER/vult-win.zip $APPVEYOR_BUILD_FOLDER/vultc.exe $APPVEYOR_BUILD_FOLDER/runtime/*.*
+7z a $APPVEYOR_BUILD_FOLDER/vult-dll.zip $APPVEYOR_BUILD_FOLDER/shared/build/vult.dll $APPVEYOR_BUILD_FOLDER/shared/build/vult.lib $APPVEYOR_BUILD_FOLDER/shared/build/vult.exp
 npm install vult -g
 vultc --help
