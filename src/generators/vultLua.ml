9c5b6855ffcacf700acf858c2a2a45f4bff5fdfa
Leonardo <modlfo@gmail.com>
2017-01-29 21:13:31 +0100

    Tries to fix the performance issues with luajit

--- src/generators/vultLua.ml
+++ src/generators/vultLua.ml
@@ -76,8 +76,8 @@ function this.floor(x)          return math.floor(x); end
 function this.tan(x)            return math.tan(x); end
 function this.tanh(x)           return math.tanh(x); end
 function this.sqrt(x)           return x; end
-function this.set(a,i,v)        a[i+1]=v; end
-function this.get(a,i)          return a[i+1]; end
+function this.set(a,i,v)        a[i]=v; end
+function this.get(a,i)          return a[i]; end
 function this.int_to_float(i)   return i; end
 function this.float_to_int(i)   return math.floor(i); end
 function this.makeArray(size,v) local a = ffi.new("double[?]",size); for i=0,size-1 do a[i]=v end return a; end
@@ -129,8 +129,9 @@ let rec printExp (params:params) (e:cexp) : Pla.t =
    | CEString(s) ->
       Pla.wrap (Pla.string "\"") (Pla.string "\"") (Pla.string s)
    | CEArray(elems) ->
+      let size = List.length elems in
       let elems_t = Pla.map_sep Pla.comma (printExp params) elems in
-      {pla|{<#elems_t#>}|pla}
+      {pla|ffi.new("double[<#size#i>]", {<#elems_t#>})|pla}
    | CECall("not",[arg]) ->
       let arg_t = printExp params arg in
       {pla|(not <#arg_t#>)|pla}
