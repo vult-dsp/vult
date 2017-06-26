5c4575363781448624381738cfd7a82066c2d580
Leonardo <modlfo@gmail.com>
2017-06-26 17:40:01 +0300

    Compiles luajit outside the vult code

--- .travis-vult.sh
+++ .travis-vult.sh
@@ -4,12 +4,13 @@ eval $(opam config env)
 # installs all dependencies
 opam install containers ppx_deriving ounit js_of_ocaml js_of_ocaml-ppx pla ollvm
 # install LuaJIT to run the Lua tests
+cd ..
 git clone https://github.com/LuaJIT/LuaJIT.git
 cd LuaJIT
 git checkout v2.1
 make
 export PATH=$PATH:`pwd`/src
-cd ..
+cd vult
 # compile Vult
 make all
 # prepare to package
