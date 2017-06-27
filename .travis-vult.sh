66e0278e68b5fa54737cebf47a97c3a0bbd7d30f
Leonardo <modlfo@gmail.com>
2017-06-27 18:07:29 +0300

    Tries to install luajit from the distro

--- .travis-vult.sh
+++ .travis-vult.sh
@@ -3,15 +3,6 @@ export OPAMYES=1
 eval $(opam config env)
 # installs all dependencies
 opam install containers ppx_deriving ounit js_of_ocaml js_of_ocaml-ppx pla ollvm
-# install LuaJIT to run the Lua tests
-git clone https://github.com/LuaJIT/LuaJIT.git
-cd LuaJIT
-git checkout v2.1
-make
-sudo make install
-cd ..
-rm -rf LuaJIT
-# compile Vult
 make all
 # prepare to package
 cp ./_build/src/vultc.native ./vultc
