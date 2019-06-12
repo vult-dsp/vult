6b4dec8aa1d1005927dc92af1b4cf0b021c21988
Leonardo <modlfo@gmail.com>
2019-06-12 20:24:06 +0300

    Improves the random name generator

--- src/util/nameTable.ml
+++ src/util/nameTable.ml
@@ -8,12 +8,16 @@ type t =
 let make () =
    { n = Random.State.make [|0|]; table = Hashtbl.create 128 }
 
+let generateChar t _ =
+   let base = if Random.State.bool t.n then 97 else 65 in
+   Char.chr (base + (Random.State.int t.n 26))
+
 let generateName (t:t) id =
    match Hashtbl.find_opt t.table id with
    | Some name -> name
    | None ->
       let is_generated = String.get id 0 = '_' in
-      let name = String.init 12 (fun _ -> Char.chr (97 + (Random.State.int t.n 26))) in
+      let name = String.init 12 (generateChar t) in
       let name = if is_generated then "_" ^ name else name in
       let () = Hashtbl.add t.table id name in
       name
