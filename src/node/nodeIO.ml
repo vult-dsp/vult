2b5762661c6d2f6893e69baf3baba4865618be06
Leonardo Laguna Ruiz <modlfo@gmail.com>
2024-01-14 19:17:13 +0200

    Updates the copyright years.

--- src/node/nodeIO.ml
+++ src/node/nodeIO.ml
@@ -1,6 +1,6 @@
 (*
    The MIT License (MIT)
-   Copyright (c) 2014 Leonardo Laguna Ruiz
+   Copyright (c) 2014-2024 Leonardo Laguna Ruiz
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
    in the Software without restriction, including without limitation the rights
--- src/vult.ml
+++ src/vult.ml
@@ -1,7 +1,7 @@
 (*
    The MIT License (MIT)
 
-   Copyright (c) 2020 Leonardo Laguna Ruizn
+   Copyright (c) 2014-2024 Leonardo Laguna Ruizn
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
